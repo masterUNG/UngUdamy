a7ea8b6179308640615c133de23b6c1c648f7f65
masterUNG <phrombutr@gmail.com>
2020-09-12 15:59:54 +0700

    Finish 12Sep

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -4,5 +4,18 @@ class MyStyle {
   Color primaryColor = Color(0xff1b5e20);
   Color darkColor = Color(0xff003300);
 
+  Widget showProgress() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget errorImage() {
+    return Container(
+      width: 120,
+      child: Image.asset('images/teacher.png'),
+    );
+  }
+
   MyStyle();
 }
--- lib/widget/my_service_student.dart
+++ lib/widget/my_service_student.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ungudamy/utility/my_constant.dart';
+import 'package:ungudamy/utility/my_style.dart';
 import 'package:ungudamy/widget/authen.dart';
+import 'package:ungudamy/widget/home_student.dart';
 
 class MyServiceStudent extends StatefulWidget {
   @override
@@ -9,17 +11,18 @@ class MyServiceStudent extends StatefulWidget {
 }
 
 class _MyServiceStudentState extends State<MyServiceStudent> {
+  Widget currentWidget = HomeStudent();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome Student'),
+        backgroundColor: MyStyle().primaryColor,
       ),
       drawer: Drawer(
         child: MyConstant().buildListTileSignOut(context),
-      ),
+      ),body: currentWidget,
     );
   }
-
-  
 }
